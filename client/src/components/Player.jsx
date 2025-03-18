8d275bb741130de0ce66bb5df6228e79733a4506
qnchoyy <qni191488@abv.bg>
2025-03-18 14:08:53 +0200

    Add basic Player component

---
+++ client/src/components/Player.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Player = () => {
+  return (
+    <div
+      className="absolute w-10 h-10 bg-blue-500 rounded-full"
+      style={{ left: "200px", top: "200px" }}
+    >
+      Player
+    </div>
+  );
+};
