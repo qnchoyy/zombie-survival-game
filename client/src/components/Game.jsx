9c9a2952de564b969fdd43c8b8f80e2318afb0a7
qnchoyy <qni191488@abv.bg>
2025-03-18 14:09:02 +0200

    Add basic Game component

---
+++ client/src/components/Game.jsx
@@ -0,0 +1,11 @@
+import { Player } from "./Player.jsx";
+
+const Game = () => {
+  return (
+    <div className="relative w-full h-screen bg-gray-800 flex items-center justify-center">
+      <Player />
+    </div>
+  );
+};
+
+export default Game;
