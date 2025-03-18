67e31dd8d0fb98ed06b07f2b270ab07d8f7a682e
qnchoyy <qni191488@abv.bg>
2025-03-18 14:09:11 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
+import Game from "./components/Game.jsx";
+
 function App() {
   return (
     <h1 className="flex items-center justify-center bg-blue-600">
       Hello world!
     </h1>
+    <div className="w-screen h-screen">
+      <Game />
+    </div>
   );
 }
 
